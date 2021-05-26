891e1a2a8d122bb633a4cdb212f5e3c5063d5b92
gabrielecaddeo <caddeogabrielemario@gmail.com>
2021-05-26 17:50:17 +0200

    A new working wrapper with the possibility of changing the position in real time

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gazebo-yarp-digit-plugin
 A tentative C++ wrapper for the Python based Digit tactile sensor simulation
+
+The folder must be contained in the same folder of the tacto folder
+
+This working version of the tacto wrapper has these features:
+- No connection to a physical environment
+- A simple python mid level called "sensor_pybind.py" which, links the "renderer.py" file of Tacto and the "main.cpp" file which uses pybind11
+- Interaction between the sensor and a single object, even if the structure allows multiple objects
+- Hard wired position in a known configuration (for simulation purpose, the object has a sinusoidal movement)
+
--- pybind/src/main.cpp
+++ pybind/src/main.cpp
@@ -1,11 +1,13 @@
 #include <cstdlib>
 #include <chrono>
+#include <cmath>
 #include <iostream>
 #include <thread>
 
 #include <opencv2/opencv.hpp>
 
 #include <pybind11/embed.h>
+#include <pybind11/stl.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
@@ -29,27 +31,48 @@ int main(int argc, char** argv)
     //initialize the interpreter
     py::scoped_interpreter guard {};
     
-    //
+    
     py::object sensor_module= py::module_::import("sensor_pybind").attr("Sensor");
     py::object sensor=sensor_module();
     
     py::object add_object= sensor.attr("add_object");
+    //intiialize position
+    //convert from std vector so that it is easiest
+    std::vector<double> position_vector_ball{-0.015, 0.0, 0.048};
+    py::list position= py::cast(position_vector_ball);
     
+    /*
+    we can use also the append method
+   
     py::list position;
     position.append(-0.015);
     position.append(0.0);
     position.append(0.048);
+    */
+
+    //initialize orientation
+    //convert from std vector so that it is easiest
+    std::vector<double> orientation_vector_ball{0.0, 0.0, 0.0};
+    py::list orientation= py::cast(orientation_vector_ball);
     
-    py::list orientation;
-    orientation.append(0.0);
-    orientation.append(0.0);
-    orientation.append(0.0);
-    py::print(position);
-    add_object("mesh"_a= "../../tacto/examples/objects/textured_sphere_smooth_meters.obj", "obj_id"_a=2, "position"_a=position, "orientation"_a= orientation);
+    //name of the object to add    
+    py::str ball= "ball";
+    //initialize the dictionaries for the python wrapper
+    py::dict dict_objects_orientation, dict_objects_position;
+    dict_objects_position[ball]= position;
+    dict_objects_orientation[ball]= orientation;
+    
+    
+    //add object
+    add_object("mesh"_a= "../../tacto/examples/objects/textured_sphere_smooth_meters.obj", "obj_name"_a=ball, "position"_a=dict_objects_position[ball], "orientation"_a= dict_objects_orientation[ball]);
+    
+    //initialize YARP port
     Network yarp;
     BufferedPort<ImageOf<PixelRgb> > port;
     port.open("/sparrow");
     cv::Mat img;
+   
+
     /*cv::Mat test = cv::imread("ciao.png", cv::IMREAD_COLOR);
     yarp::sig::ImageOf<yarp::sig::PixelRgb> img_yarp = yarp::cv::fromCvMat<PixelRgb>(test);
     std::cout << test.cols << " " << test.rows << std::endl;
@@ -60,24 +83,40 @@ int main(int argc, char** argv)
     std::cout << (int) pixel.r << " " << (int)pixel.g << " " << (int)pixel.b << std::endl;*/
     while (true)
     {
-        //sensor.attr("get_image")();ls
-
-        py::array_t<uint8_t> rgb= sensor.attr("get_image")();
+        //here we could update pos and orientation
+        
+        //position_vector_ball[2]+=0.0001;
+        //position= py::cast(position_vector_ball);
+        //dict_objects_position[ball]=position;
+        //py::print(position);
+        
+        //get the image
+        py::array_t<uint8_t> rgb= sensor.attr("get_image")("objects_positions"_a=dict_objects_position, "objects_orientations"_a= dict_objects_orientation);
+        
+        //convert the image
         img = cv::Mat(rgb.shape(0), rgb.shape(1), CV_8UC3, (unsigned char*)rgb.data());
-            
+
         //cv::cvtColor(img, img, cv::COLOR_RGB2BGR);
 
         //exit(0); 
         
         
         //yarp::cv::toCvMat(output)=img;
+        
+        //prepare the output and convert fromCVMat
         ImageOf<PixelRgb>& output = port.prepare();
         output=yarp::cv::fromCvMat<PixelRgb>(img);  
+
+
         //std::cout << output.width() << " " << output.height() << std::endl;
         //auto pixel = output.pixel(10, 10);
         //std::cout << (int) pixel.r << " " << (int)pixel.g << " " << (int)pixel.b << std::endl;
+
+        //write into the port
         port.write();        
         
+
+        //useful just for simulation purpose
         std::this_thread::sleep_for(300ms);
     }
     return EXIT_SUCCESS;
